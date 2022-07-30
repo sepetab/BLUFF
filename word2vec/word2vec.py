75094175ed399df15025bd659d42787fcaaa0f12
Fatemeh Karbaschi <82693000+Fatimah-bit@users.noreply.github.com>
2022-07-30 17:28:07 +1000

    Update word2vec.py

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -1,12 +1,7 @@
-# math and parse
 import numpy as np
 import pandas as pd
-import json
-#visualisation
 import seaborn as sns
-# plotting
 import matplotlib.pyplot as plt
-#natural language toolkit
 import nltk
 from sklearn.preprocessing import LabelBinarizer
 from nltk.corpus import stopwords
@@ -16,24 +11,18 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize,sent_tokenize
 from bs4 import BeautifulSoup
 import re,string,unicodedata
-#from keras.preprocessing import text, sequence
+from keras.preprocessing import text, sequence
 from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 from sklearn.model_selection import train_test_split
 from string import punctuation
-from gensim.models import Word2Vec
-#import torchtext
-#from torchtext.data import get_tokenizer
-from nltk.tokenize import word_tokenize
-#from torchtext.data.utils import get_tokenizer
-#from torchtext.vocab import build_vocab_from_iterator 
-#from torchnlp.encoders.text import StaticTokenizerEncoder,stack_and_pad_tensors,pad_tensor
-from torch.nn.utils.rnn import pad_sequence
+import keras
+from keras.models import Sequential
+from keras.layers import Dense,Embedding,LSTM,Dropout,Bidirectional,GRU
+import tensorflow as tf
+import gensim
+from keras_preprocessing.sequence import pad_sequences
 
 nltk.download('punkt')
-#import keras
-#from keras.models import Sequential
-#from keras.layers import Dense,Embedding,LSTM,Dropout,Bidirectional,GRU
-#import tensorflow as tf
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', -1)
@@ -42,69 +31,93 @@ data = pd.read_json("./Sarcasm_Headlines_Dataset_v2.json", lines=True)
 
 #deleting article_link column
 del data['article_link']
+# Data analysis
 
-# number of non sarcastic headlines
-#print (data.shape[0]-data.is_sarcastic.sum())
+train = pd.read_csv("./train.csv")
+test = pd.read_csv("./test.csv")
 
-# number of sarcastic headlines
-#print(data.is_sarcastic.sum())
+stop = set(stopwords.words('english'))
+punctuation = list(string.punctuation)
+stop.update(punctuation)
 
-# remove stopwords as they don't add any meanings to the sentece
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
 
-stop = set(stopwords.words('English'))
-punctuation = list (string.punctuation)
-stop.update (punctuation)
-
-# stopword and punctuation in English language
-#print (stop)
-
+#Removing the square brackets
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+# Removing URL's
+def remove_between_square_brackets(text):
+    return re.sub(r'http\S+', '', text)
+#Removing the stopwords from text
 def remove_stopwords(text):
     final_text = []
     for i in text.split():
         if i.strip().lower() not in stop:
             final_text.append(i.strip())
     return " ".join(final_text)
+#Removing the noisy text
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    text = remove_stopwords(text)
+    return text
+#Apply function on review column
 
-def denoise_data (row):
-    row = remove_stopwords(row)
-    return row
+train['headline']= train['headline'].apply(denoise_text)
+test['headline']= test['headline'].apply(denoise_text)
 
+words = []
+for i in train.headline.values:
+    words.append(i.split())
+words[:5]
 
-data ['headline'] = data['headline'].apply (denoise_data)
-#print (data.head())
-
-# wordcloud for sarcastic text
-'''
-plt.figure(figsize = (20,20)) 
-wc = WordCloud(max_words = 2000 , width = 1600 , height = 800).generate(" ".join(data[data.is_sarcastic == 1].headline))
-plt.imshow(wc , interpolation = 'bilinear')
-plt.title('Wordcloud for sarcastic headlines')
-plt.savefig('Wordcloud for sarcastic headlines.png')
-
-#wordcloud for non sarcastic text
-plt.figure(figsize = (20,20)) 
-wc = WordCloud(max_words = 2000 , width = 1600 , height = 800).generate(" ".join(data[data.is_sarcastic == 0].headline))
-plt.imshow(wc , interpolation = 'bilinear')
-plt.title('Wordcloud for non sarcastic headlines')
-plt.savefig('Wordcloud for non sarcastic headlines.png')
-'''
-train = pd.read_csv("./train.csv")
-#train['headline'].dropna(inplace=True)
-test = pd.read_csv("./test.csv")
-# Use gensim for modeling
-# gensim is an open source library for unsupervised topic modeling 
-
-
-tokens = [word_tokenize(sentences) for sentences in train]    
-    
 EMBEDDING_DIM = 200
-#Creating Word Vectors by Word2Vec Method from genism library
-w2v_model = Word2Vec(tokens , vector_size=EMBEDDING_DIM , window = 5 , min_count = 1)
 
-#building vocabulary for training 
-w2v_model.build_vocab(tokens)
-print("\n Training the word2vec model...\n")
-w2v_model.train(tokens, total_examples = len(tokens), epochs = 4000)
+#Creating Word Vectors by Word2Vec Method (takes time...)
+w2v_model = gensim.models.Word2Vec(sentences = words , vector_size=EMBEDDING_DIM , window = 5 , min_count = 1)
+
+#vocab size
+#len(w2v_model.wv.vocab)
+
+tokenizer = text.Tokenizer(num_words=35000)
+tokenizer.fit_on_texts(words)
+tokenized_train = tokenizer.texts_to_sequences(words)
+x = pad_sequences(tokenized_train, maxlen = 20)
+
+vocab_size = len(tokenizer.word_index) + 1
+vocab = w2v_model.wv.key_to_index.keys()
+
+def get_weight_matrix(model, vocab):
+    # total vocabulary size plus 0 for unknown words
+    vocab_size = len(vocab) + 1
+    # define weight matrix dimensions with all 0
+    weight_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
+    # step vocab, store vectors using the Tokenizer's integer mapping
+    for word, i in vocab.items():
+        weight_matrix[i] = w2v_model.wv[word]
+    return weight_matrix
+
+embedding_vectors = get_weight_matrix(w2v_model, tokenizer.word_index)
+
+#Training model
+model = Sequential()
+#Non-trainable embeddidng layer
+model.add(Embedding(vocab_size, output_dim=EMBEDDING_DIM, weights=[embedding_vectors], input_length=20, trainable=True))
+#LSTM 
+model.add(Bidirectional(LSTM(units=128 , recurrent_dropout = 0.3 , dropout = 0.3,return_sequences = True)))
+model.add(Bidirectional(GRU(units=32 , recurrent_dropout = 0.1 , dropout = 0.1)))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer=keras.optimizers.Adam(lr = 0.01), loss='binary_crossentropy', metrics=['acc'])
+
+del embedding_vectors
+
+x_train, x_test, y_train, y_test = train['headline'],test['headline'],train['is_sarcastic'],test['is_sarcastic']
+
+history = model.fit(x_train, y_train, batch_size = 128 , validation_data = (x_test,y_test) , epochs = 3)
+
+print("Accuracy of the model on Training Data is - " , model.evaluate(x_train,y_train)[1]*100)
+print("Accuracy of the model on Testing Data is - " , model.evaluate(x_test,y_test)[1]*100)
 
 
-#max_dataset_size = len(w2v_model.wv.syn0)
